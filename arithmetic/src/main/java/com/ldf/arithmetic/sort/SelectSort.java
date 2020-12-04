35c210963fcf79d61866927fb84b3b5333765a3a
lidefu <18354236885@qq.com>
2020-12-04 21:34:27 +0800

    feat:选择排序

--- arithmetic/src/main/java/com/ldf/arithmetic/sort/SelectSort.java
+++ arithmetic/src/main/java/com/ldf/arithmetic/sort/SelectSort.java
@@ -8,16 +8,31 @@ public class SelectSort extends AbstractSort{
 
     /**
      * 选择排序
+     * 找到最大值放在首位，然后再依次在剩余元素中找到最大值，放在第二位，依次。。
      */
 
     public static void main(String[] args) {
         AbstractSort sort = new QuickSort();
-        sort.test(10);
+        sort.test(10000);
     }
 
     @Override
     public void sort(int nums[]){
-
+        for(int i=0; i<= nums.length-1; i++){
+            int min = i;
+            //获取当前最小值的坐标
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[min] > nums[j]){
+                    min = j;
+                }
+            }
+            //交换最小值至i处
+            if(i != min){
+                int temp = nums[i];
+                nums[i] = nums[min];
+                nums[min] = temp;
+            }
+        }
     }
 
 }
