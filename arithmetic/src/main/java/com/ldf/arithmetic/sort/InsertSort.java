b18af02f3a38a3acd3e93af076fc90a99a967984
lidefu <18354236885@qq.com>
2020-12-07 23:11:18 +0800

    feat:插入排序

--- arithmetic/src/main/java/com/ldf/arithmetic/sort/InsertSort.java
+++ arithmetic/src/main/java/com/ldf/arithmetic/sort/InsertSort.java
@@ -8,6 +8,7 @@ public class InsertSort extends AbstractSort {
 
     /**
      * 插入排序
+     * 保持已遍历部分有序，遍历至新元素时，与前边有序数据
      */
 
     public static void main(String[] args) {
@@ -17,7 +18,19 @@ public class InsertSort extends AbstractSort {
 
     @Override
     public void sort(int nums[]){
-
+        for(int i=1; i<nums.length; i++){
+            int temp = nums[i];
+            int j=i;
+            while (j>0 && temp<nums[j-1]){
+                //右移
+                nums[j] = nums[j-1];
+                j--;
+            }
+            //插入指定位置
+            if(j!=i){
+                nums[j] = temp;
+            }
+        }
     }
 
 }
